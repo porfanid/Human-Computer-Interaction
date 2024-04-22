c7cf668df1ffbb52cfd5825042b5facc2c334402
Pavlos Orfanidis <pavlos@orfanidis.net.gr>
2024-04-22 03:29:06 +0300

    Added bsic files to support movement

---
+++ HCI/Assets/CameraMovement.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class CameraMovement : MonoBehaviour
+{
+    public float moveSpeed = 200f;   // Speed of camera movement
+    public float mouseSensitivity = 10f; // Mouse sensitivity
+    private Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
+
+    private float rotateX;
+    private float rotateY;
+    
+
+    void Start(){
+        rb = GetComponent <Rigidbody>();
+    }
+
+    void OnMove (InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x; 
+        movementY = movementVector.y; 
+    }
+
+    void OnLook (InputValue movementValue)
+    {
+        Debug.Log("Hello World");
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        rotateX = movementVector.x; 
+        rotateY = movementVector.y;
+    }
+
+    private void FixedUpdate() 
+    {
+        Vector3 forward = transform.forward;
+    forward.y = 0.0f; // Ignore vertical component for movement on the XZ plane
+    forward.Normalize();
+
+    Vector3 right = transform.right;
+    right.y = 0.0f; // Ignore vertical component for movement on the XZ plane
+    right.Normalize();
+
+    Vector3 movement = (forward * movementY + right * movementX) * moveSpeed * Time.fixedDeltaTime;
+
+    // Apply movement to the object's position using transform
+    transform.Translate(movement, Space.World);
+
+        Vector3 rotation = new Vector3(rotateY, rotateX, 0.0f) * mouseSensitivity * Time.fixedDeltaTime;
+        transform.eulerAngles-=rotation;
+    }
+}
